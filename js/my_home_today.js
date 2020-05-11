d4185c2b21d9997bc26e9f6a786bf4f26baa8b22
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-11 16:01:09 +0300

    Update my_home_today.js

--- js/my_home_today.js
+++ js/my_home_today.js
@@ -1,14 +1,14 @@
 // Your web app's Firebase configuration
-			var firebaseConfig = {
-				apiKey: "AIzaSyAvP3cRKmoJvCwcJYYJLRe_ARPN1_wngYo",
-				authDomain: "sdy60ge5a5.firebaseapp.com",
-				databaseURL: "https://sdy60ge5a5.firebaseio.com",
-				projectId: "sdy60ge5a5",
-				storageBucket: "sdy60ge5a5.appspot.com",
-				messagingSenderId: "96930165171",
-				appId: "1:96930165171:web:1c766954e21cc757d09bc6",
-				measurementId: "G-H3SGFNEN3P"
-			};
+var firebaseConfig = {
+	apiKey: "AIzaSyAvP3cRKmoJvCwcJYYJLRe_ARPN1_wngYo",
+	authDomain: "sdy60ge5a5.firebaseapp.com",
+	databaseURL: "https://sdy60ge5a5.firebaseio.com",
+	projectId: "sdy60ge5a5",
+	storageBucket: "sdy60ge5a5.appspot.com",
+	messagingSenderId: "96930165171",
+	appId: "1:96930165171:web:1c766954e21cc757d09bc6",
+	measurementId: "G-H3SGFNEN3P"
+	};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
