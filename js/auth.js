491d4e8f7a778c199853367c1df2d17736aeba63
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-12 02:05:23 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -14,7 +14,7 @@ const uiConfig = {
   signInFlow: 'popup',
   signInSuccessUrl: 'my_home_today.html',
   signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     //firebase.auth.GithubAuthProvider.PROVIDER_ID,
     //Additional login options should be listed here
