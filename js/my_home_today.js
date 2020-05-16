107b024bf0dc3138937ca48bd7be8330d71adb2f
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-16 17:33:26 +0300

    Update my_home_today.js

--- js/my_home_today.js
+++ js/my_home_today.js
@@ -44,7 +44,7 @@ function readUserData(name){
 	
   });
   //console.log("The first record is: "+labelArray[0]+" : "+dataArray[0]);
-  var dailyTotal = dataArray[labelArray.length];
+  var dailyTotal = dataArray[labelArray.length-1];
   console.log("Total consumption is: "+dailyTotal+" kWh");
   var hoursOfDay = labelArray.length - 1;
   
