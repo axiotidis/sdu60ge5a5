1fc13f09e11725a110bb51e9a360a357220a81d4
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-17 14:38:29 +0300

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,3 +1,22 @@
+//Listen for form submit
+document.getElementById('selectMonth').addEventListener('submit', submitForm);
+
+/Submit form
+function submitForm(e){
+    e.preventDefault();
+	var selectedMonth = getInputVal('sMonth');
+	if (selectedMonth != "Choose a month"){
+		alert("Selected Month is: "+ selectedMonth);
+	}
+	
+}
+
+
+/*
+
 var e = document.getElementById('sMonth');
+
 var selectedMonth = e.options[e.selectedIndex].value;
 alert("Selected Month is: "+ selectedMonth);
+
+*/
