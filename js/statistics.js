03ae9a927a6c170cb546af36e257a57ad50583a4
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-17 19:44:01 +0300

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -48,6 +48,21 @@ firebase.auth().onAuthStateChanged(function(user) {
 					
 	});
 	
+function readMonthlyTotal(name, month){
+	var mDataArray = [];
+	var mRootRef = firebase.database().ref();
+	var mUrlRef = dRootRef.child("users/"+name+"/consumption/monthly/totals/"+month+"/");
+	mUrlRef.once("value", function(snapshot) {
+		snapshot.forEach(function(child) {
+		//console.log(child.key+": "+child.val());
+		mDataArray = snapshotDataToArray(snapshot);
+		});
+		
+		var totalForThisMonth = parseFloat(mDataArray[0]).toFixed(2);
+		document.getElementById("consumtion").innerHTML = totalForThisMonth + " kWh";
+
+}	
+	
 function readMonthlyPerDevice(name, month){
 	var dDataArray = [];
 	var dLabelArray = [];
