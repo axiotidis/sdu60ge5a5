51548f5fb6183026efa08b5a6f66bd34d4f177ec
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-17 18:38:51 +0300

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,6 +1,10 @@
-var selectedMonth = "";
-		function monthFunction(month){
-			selectedMonth = month;
-			document.getElementById("month").innerHTML = "in "+selectedMonth;
-			alert("Selected month is: "+selectedMonth);
-		}
+var d = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var selectedMonth = d.getMonth();
+
+function monthFunction(month){
+	selectedMonth = month;
+	document.getElementById("month").innerHTML = "in "+selectedMonth;
+	//alert("Selected month is: "+selectedMonth);
+	}
