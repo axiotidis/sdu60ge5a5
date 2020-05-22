926ccee2253b163ee488567079c8823b883cf960
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-22 18:19:51 +0300

    Add files via upload

---
+++ js/hintsandtips.js
@@ -0,0 +1,28 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyAvP3cRKmoJvCwcJYYJLRe_ARPN1_wngYo",
+	authDomain: "sdy60ge5a5.firebaseapp.com",
+	databaseURL: "https://sdy60ge5a5.firebaseio.com",
+	projectId: "sdy60ge5a5",
+	storageBucket: "sdy60ge5a5.appspot.com",
+	messagingSenderId: "96930165171",
+	appId: "1:96930165171:web:1c766954e21cc757d09bc6",
+	measurementId: "G-H3SGFNEN3P"
+	};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+let database = firebase.database();
+
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		user = firebase.auth().currentUser;
+		var name = user.displayName;
+		
+		
+		} else {
+					location.replace("index.html");
+				}
+					
+	});
