51f679bbf7e4aa5a467292d2eb1563d5be713c02
axiotidis <50782505+axiotidis@users.noreply.github.com>
2020-05-22 13:16:21 +0300

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -103,8 +103,10 @@ for (var j = 0; j < numberOfusers; ++j){
 		var ref = firebase.database().ref("users/"+userArray[j]+"/profile/points").once('value').then(function(snapshot) {
 		  var pointVal = snapshot.val();
 		  console.log("pointVal= "+pointVal);
-		  myChart.data.dataset.push(pointVal);
-		  myChart.update();
+		  myChart.data.datasets.forEach((dataset) => {
+		dataset.data.push(pointsArray[i]);
+	});
+		myChart.update();
 		  
 	});
 	
